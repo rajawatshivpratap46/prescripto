eb116371fe0b48867bedb8cff62dc572abeea92c
rajawatshivpratap46 <shiva18021912@gmail.com>
2025-05-23 21:43:25 +0530

    Fix: defined missing 'assets' variable in About.jsx

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import {assets} from '../assets/assets_frontend/assets'
 const About = () => {
   return (
     <div>
-      
+        <div className='text-center text-2xl pt-10 text-gray-500'>
+             <p>ABOUT <span className='text-gray-700 font-medium'>US</span></p>
+        </div>
+
+        <div className='my-10 flex flex-col md:flex-row gap-12'>
+            <img className='w-full md:max-w-[360px]' src ={assets.about_image} alt=""/>
+            <div>
+                <p>Welcome to Prescripto, your trusted partner in managing your healthcare needs conveniently and efficiently. At Prescripto, we understand the challenges individuals face when it comes to scheduling doctor appointments and managing their health records</p>
+                <p>Prescripto is committed to excellence in healthcare technology. We continuously strive to enhance our platform, integrating the latest advancements to improve user experience and deliver superior service. Whether you're booking your first appointment or managing ongoing care, Prescripto is here to support you every step of the way</p>
+                <b>Our Vision</b>
+                <p>Prescripto is committed to excellence in healthcare technology. We continuously strive to enhance our platform, integrating the latest advancements to improve user experience and deliver superior service. Whether you're booking your first appointment or managing ongoing care, Prescripto is here to support you every step of the way</p>
+            </div>
+        </div>
     </div>
   )
 }
