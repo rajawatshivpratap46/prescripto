74e9eca840ee41c79546ecb450fcf5465110cd82
rajawatshivpratap46 <shiva18021912@gmail.com>
2025-05-26 11:35:54 +0530

    Normalize line endings to LF

---
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { assets } from '../../assets/assets_admin/assets'
+
+
+const AddDoctor = () => {
+  return (
+    <form>
+      <p>Add Doctors</p>
+      <div>
+          <div>
+              <label htmlFor="doc-img">
+                  <img src={assets.upload_area} alt=""/>
+              </label>
+                <input type="file" id="doc-img" hidden/>
+                <p>Upload doctor <br/>picture</p>
+          </div>
+
+          <div>
+              <div>
+
+                  <div>
+                      <p> Doctor Name</p>
+                      <input type="text" placeholder='Name' required/>
+                  </div>
+
+                  <div>
+                      <p> Doctor Email</p>
+                      <input type="email" placeholder='Email' required/>
+                  </div>
+
+                  <div>
+                      <p> Doctor Password</p>
+                      <input type="password" placeholder='password' required/>
+                  </div>
+
+                  <div>
+                      <p> Experience</p>
+                      <select name="" id="">
+                          <option value ="1 Year"></option>  
+                          <option value ="2 Year"></option>
+                          <option value ="3 Year"></option>
+                          <option value ="4 Year"></option>
+                          <option value ="5 Year"></option>
+                          <option value ="6 Year"></option>
+                          <option value ="7 Year"></option>
+                          <option value ="8 Year"></option>
+                          <option value ="9 Year"></option>
+                          <option value ="10 Year"></option>
+                      </select>
+                  </div>
+
+                  <div>
+                      <p> Fees</p>
+                      <input type="number" placeholder='fees' required/>
+                  </div>
+
+              </div>
+            </div>
+
+      </div>
+    </form>
+  )
+}
+
+export default AddDoctor
+
