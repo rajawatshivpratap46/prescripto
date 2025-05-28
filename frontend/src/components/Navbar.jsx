ed496b3a35404e5f19d4e6cd90b23643c4930da5
rajawatshivpratap46 <shiva18021912@gmail.com>
2025-05-28 23:33:53 +0530

    nothing

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -18,16 +18,16 @@ const Navbar = () => {
     }
 
   return (
-    <div className='flex items-center justify-between text-sm py-4 mb-5 border-b-gray-400'>
+    <div className='flex items-center justify-between text-sm py-4 mb-5 border border-gray-400'>
       <img onClick={()=>navigate('/')} className='w-44 cursor-pointer' src={assets.logo} alt =""/>
-      <ul className ='hidden md:flex items-start gap-5 font-medium'>
+      <ul className='hidden md:flex items-start gap-5 font-medium'>
          <NavLink to='/'>
             <li className='py-1'>Home</li>
-            <hr className ="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden"/>
+            <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden"/>
          </NavLink>
          <NavLink to='/doctors'>
             <li className='py-1'>ALL DOCTORS</li>
-            <hr className ='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
+            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
          </NavLink>
          <NavLink to='/about'>
             <li className ='py-1'>ABOUT</li>
@@ -35,7 +35,7 @@ const Navbar = () => {
          </NavLink>
          <NavLink to='/contact'>
             <li className='py-1'>CONTACT</li>
-            <hr className ='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
+            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
          </NavLink>
       </ul>
       <div className='flex items-center gap-4'>
@@ -46,8 +46,8 @@ const Navbar = () => {
              <img className='w-2.5' src={assets.dropdown_icon} alt=""/>
              <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
                <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
-                  <p onClick={()=>navigate('my-profile')}className='hover:text-black cursor-pointer'>My profile</p>
-                  <p onClick={()=>navigate('my-appointments')}className='hover:text-black cursor-pointer'>My Appointments</p>
+                  <p onClick={()=>navigate('/my-profile')}className='hover:text-black cursor-pointer'>My profile</p>
+                  <p onClick={()=>navigate('/my-appointments')}className='hover:text-black cursor-pointer'>My Appointments</p>
                   <p onClick={ logout}className='hover:text-black cursor-pointer'>Logout</p>
                </div>
              </div>
