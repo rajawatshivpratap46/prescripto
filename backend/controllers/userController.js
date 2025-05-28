62e801dc3295e2d0b34c091fdfeb37b57c781257
rajawatshivpratap46 <shiva18021912@gmail.com>
2025-05-28 23:14:46 +0530

    what the life is

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
-
-
-
+import validator from 'validator';
+import bcrypt from 'bcrypt'
+import userModel from '../models/userModel.js';
+import jwt from 'jsonwebtoken';
 
 
 // API to register user
@@ -8,7 +9,71 @@ const registerUser = async (req, res) => {
 
     try{
 
-    } catch(error){
+        const {name,email,password} = req.body
+
+        if(!name || !email || !password){
+            return res.json({message: "Missing Details"})
+        }
+
+        // validating email format
+        if(!validator.isEmail(email)){
+            return res.json({message: "enter a valid email"})
+
+        }
+         // validating password strength
+        if(password.length < 8){
+            return res.json({message: "enter a strong password"})
+
+        }
+
+        // Hashing the password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        const userData= {
+            name,
+            email,
+            password: hashedPassword
+        }
+
+        const newUser =new userModel(userData)
+        const user = await newUser.save()
         
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET)
+
+        res.json({success: true,token})
+
+
+    } catch(error){
+        console.log(error)
+        res.json({success: false, message:error.message})
     }
 }
+
+// API for user login
+const loginUser = async (req, res) => {
+    try {
+
+        const {email,password} = req.body
+        const user = await userModel.findOne({ email })
+
+        if(!user){
+            return res.json({ success: false, message:'User does not exist' })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if(!isMatch){
+            const token = jwt.sign({id: user._id}, process.env.JWT_SECRET)
+            res.json({success:true,token})
+        } else{
+            res.json({success:false,message:"Invalid credentials"})
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
+}
+
+export {registerUser,loginUser}
